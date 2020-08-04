ca2d4fd7392e45022c09a50776067fef0c883754
maxpoin <nawafi.suren@gmail.com>
2020-08-04 22:57:44 +0700

    first commit

---
+++ application/helpers/custom_helper.php
@@ -0,0 +1,4 @@
+<?php
+function template($a=''){
+	return base_url('assets/templates/gentelella/'.$a);
+}
---
+++ application/views/layouts/html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include 'head.php'?>
+	<body class="nav-md">
+	<div class="container body">
+	  <div class="main_container">
+	  	<?php include 'sidebar.php';?>
+	  	<?php include 'header.php';?>
+
+        <!-- page content  <?=$content?> -->
+        <div class="right_col" role="main">
+          69
+        </div>
+        <?php include 'footer.php'?>
+	  </div>
+	</div>
+	<?php include 'javascript.php'?>
+	</body>
+</html>
